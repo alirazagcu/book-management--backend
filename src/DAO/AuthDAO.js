cbb7f4ba8026c6d996a675ef2ebc10817bcd07f0
Ali Raza <ali.backend@fit-outfit.com>
2021-04-19 00:36:54 +0500

    fixed

--- src/DAO/AuthDAO.js
+++ src/DAO/AuthDAO.js
@@ -1,16 +1,6 @@
 const { throwError } = require('../utils/Common');
 
 class AuthDAO {
-
-  // async findUserByEmail(collection, email) {
-  //   let user = await db.collection(collection)
-  //     .where('email', '==', email)
-  //     .limit(1)
-  //     .get();
-  //   if (user.empty) throwError(401, "Credentials Invalid");
-  //   return user.docs[0];
-  // }
-
 }
 
 module.exports = AuthDAO;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,24 +1,11 @@
 const { throwError } = require('../utils/Common');
 const { decrypt, generateJWT } = require('../utils/AuthUtils');
-const AuthDAO = require('../dao/AuthDAO');
+const AuthDAO = require('../DAO/AuthDAO');
 const authDAO = new AuthDAO();
 
 class AuthService {
 
   async getTokens(payload) {
-    // let { email, password } = payload;
-    // let userType = payload.user_type;
-    // let collection = userType + 's';
-    // let user = await authDAO.findUserByEmail(collection, email);
-    // if (!user.get('password')) throwError(401, "Credentials Invalid");
-    // let decryptedPassword = decrypt(user.get('password'));
-    // if (password !== decryptedPassword) {
-    //   throwError(401, "Credentials Invalid");
-    // }
-    // let token = await generateJWT(user.get('id'), user.get('email'), user.get('number'), userType);
-    // let refreshToken = await generateRefreshJWT(user.get('id'), user.get('email'), user.get('number'), 
-    //   userType);
-    // return { token, refreshToken };
   }
 }
 
